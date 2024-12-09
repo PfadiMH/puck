51cccb0f70f4c2c44991f60738661a8691027ddc
05kl <kristijan.lisica@edu.tbz.ch>
2024-12-09 11:21:41 +0100

    separate HeadingComponent into own file

---
+++ components/HeadingComponent.tsx
@@ -0,0 +1,34 @@
+import { ComponentConfig } from "@measured/puck";
+import React from "react";
+
+export type HeadingComponentProps = {
+  text: string,
+  textAlign: any,
+};
+
+function HeadingComponent({text, textAlign}: HeadingComponentProps){
+  return ( 
+    <div style={{padding: 64}}>
+      <h1 style={{ textAlign }}>{text}</h1>
+    </div> 
+  )
+};
+
+export const headingComponentConfig: ComponentConfig<HeadingComponentProps> = {
+  render: HeadingComponent,
+  fields:{
+    text: {type: "text"},
+    textAlign: {
+      type: "radio",
+      options: [
+        { label: "Left", value: "left" },
+        { label: "Center", value: "center" },
+        { label: "Right", value: "right" },
+      ],
+    },
+  },
+  defaultProps: {
+    text: "HeadingComponent",
+    textAlign: "center",
+  }
+}
--- puck.config.tsx
+++ puck.config.tsx
@@ -1,8 +1,10 @@
 import type { Config } from "@measured/puck";
 import type { TextProps } from "./components/Text";
 import Text from "./components/Text";
+import { headingComponentConfig } from "./components/HeadingComponent";
 
 type Props = {
+  HeadingComponent: { text: string, textAlign: string }
   HeadingBlock: { title: string };
   Text: TextProps;
 };
@@ -30,28 +32,7 @@ export const config: Config<Props> = {
         </div>
       ),
     },
-    HeadingComponent:{
-      fields:{
-        text: {type: "text"},
-        textAlign: {
-          type: "radio",
-          options: [
-            { label: "Left", value: "left" },
-            { label: "Center", value: "center" },
-            { label: "Right", value: "right" },
-          ],
-        },
-      },
-      defaultProps: {
-        text: "HeadingComponent",
-        textAlign: "center",
-      },
-      render: ({ text, textAlign }) => (
-        <div style={{ padding: 64 }}>
-          <h1 style={{ textAlign }}>{text}</h1>
-        </div>
-      ),
-    },
+    HeadingComponent: headingComponentConfig,
   },
 };
 
