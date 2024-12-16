1f5b86ef8ed132462b0e70e029a9a8fec9d5cefb
Cuerillius <cyrill_weber@hotmail.com>
2024-12-16 11:12:36 +0100

    refactor conditional rendering

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,6 @@ import { ComponentConfig } from "@measured/puck";
 import Image from "next/image";
 import React from "react";
 import { SectionDivider } from "../Assets/SectionDivider";
-import Zeichnung from "../Assets/Zeichnung.svg";
 
 
 export type HeroProps = {
@@ -13,20 +12,20 @@ export type HeroProps = {
 function Hero({ title, url }: HeroProps) {
   return (
     <div className="w-full h-96 relative flex flex-col justify-center overflow-hidden items-center">
-      {url ? (
+      {url && (
         <Image
           fill
           src={url}
           alt="Hero Image"
           objectFit="cover"
         />
-      ) : null}
-      {title ? (
+      )}
+      {title && (
         <>
           <h1 className="text-4xl font-bold text-center z-10 text-white">{title}</h1>
           <div className="bg-black opacity-15 absolute z-5 w-full h-full" />
         </>
-      ) : null}
+      )}
       <SectionDivider fill={"#210002"} className="z-20 absolute bottom-0" />
     </div>
   )
