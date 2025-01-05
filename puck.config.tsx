ad9dbe6dfb7e244a1b6a5aecc0f49be94ad91c65
gabriel <gabriel.arocha@noseryoung.com>
2025-01-05 19:12:51 +0100

    make flex component

--- puck.config.tsx
+++ puck.config.tsx
@@ -1,14 +1,18 @@
-import type { Config } from "@measured/puck";
+import { DropZone, type Config } from "@measured/puck";
 import type { TextProps } from "./components/Text";
 import Text from "./components/Text";
+import { Flex, FlexProps } from "./components/Flex";
 
 type Props = {
   HeadingBlock: { title: string };
   Text: TextProps;
+  Flex: FlexProps;
 };
 
-export const config: Config<Props> = {
+
+const config: Config<Props> = {
   components: {
+    Flex, 
     Text: {
       render: Text,
       fields: {
