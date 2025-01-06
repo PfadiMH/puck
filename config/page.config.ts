978440d732ee1e932c0205f28dc8bc7f9009d1be
05kl <kristijan.lisica@edu.tbz.ch>
2025-01-06 08:55:09 +0100

    added changes from puck.config.tsx to config/page.config.ts

--- config/page.config.ts
+++ config/page.config.ts
@@ -3,14 +3,19 @@ import type { TextProps } from "@components/Text";
 import { textConfig } from "@components/Text";
 import { heroConfig } from "@components/Hero";
 import type { HeroProps } from "@components/Hero";
+import { HeadingProps} from "@components/Heading";
+import { headingConfig } from "@components/Heading";
 
 type Props = {
+
+    Heading: HeadingProps
     Text: TextProps;
     Hero: HeroProps;
 };
 
 export const config: Config<Props> = {
     components: {
+        Heading: headingConfig,
         Text: textConfig,
         Hero: heroConfig,
     },
