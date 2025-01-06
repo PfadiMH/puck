8b1557d2792ee0a46cb26b9e4a2e6c023bbb2204
gabriel <gabriel.arocha@noseryoung.com>
2025-01-06 09:01:07 +0100

    remove unused files and improve code consistency

--- components/Flex.tsx
+++ components/Flex.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 import { ComponentConfig, DropZone } from "@measured/puck";
-import getClassNameFactory from "../lib/get-class-name-factory";
-
-const getClassName = getClassNameFactory("Flex");
 
 export type FlexProps = {
   items: { minItemWidth?: number }[];
   minItemWidth: number;
 };
 
+function Flex({ items, minItemWidth }: FlexProps) {
+  return (
+    <div>
+      {items.map((item, idx) => (
+        <div
+          key={idx}
+          className="flex-item"
+          style={{ minWidth: item.minItemWidth || minItemWidth }}
+        >
+          <DropZone zone={`item-${idx}`} />
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export const flexConfig: ComponentConfig<FlexProps> = {
+  render: Flex,
   fields: {
     items: {
       type: "array",
@@ -32,17 +46,4 @@ export const flexConfig: ComponentConfig<FlexProps> = {
     items: [{}, {}],
     minItemWidth: 356,
   },
-  render: ({ items, minItemWidth }) => (
-    <div>
-      {items.map((item, idx) => (
-        <div
-          key={idx}
-          className={getClassName("item")}
-          style={{ minWidth: item.minItemWidth || minItemWidth }}
-        >
-          <DropZone zone={`item-${idx}`} />
-        </div>
-      ))}
-    </div>
-  ),
 };
