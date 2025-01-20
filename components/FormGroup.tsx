9e94e4fff65a66dda63bbbcf4450129973a49572
cediackermann <ackermanncedi@gmail.com>
2025-01-20 08:30:24 +0100

    Sign up button can now be customized

--- components/FormGroup.tsx
+++ components/FormGroup.tsx
@@ -5,10 +5,11 @@ import { ComponentConfig } from "@measured/puck";
 
 export type FormGroupProps = {
   formFields: { label: string; size: number }[];
-  form_recipient_email: string;
+  buttonLabel: string;
+  formRecipientEmail: string;
 };
 
-export function FormGroup({ formFields }: FormGroupProps) {
+export function FormGroup({ formFields, buttonLabel }: FormGroupProps) {
   const groupedFields: { label: string; size: number }[][] = [];
   let tempRow: { label: string; size: number }[] = [];
   formFields.forEach((field, index) => {
@@ -35,7 +36,7 @@ export function FormGroup({ formFields }: FormGroupProps) {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
+    <form action={"/"}>
       <table>
         <thead>
           <tr>
@@ -70,7 +71,7 @@ export function FormGroup({ formFields }: FormGroupProps) {
                 className='bg-gradient-to-br relative group/btn from-black dark:from-zinc-900 dark:to-zinc-900 to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]'
                 type='submit'
               >
-                Sign up &rarr;
+                {buttonLabel}
               </button>
             </td>
           </tr>
@@ -100,12 +101,16 @@ export const formGroupConfig: ComponentConfig<FormGroupProps> = {
         size: 1,
       },
     },
-    form_recipient_email: {
+    buttonLabel: {
+      type: "text",
+    },
+    formRecipientEmail: {
       type: "text",
     },
   },
   defaultProps: {
     formFields: [{ label: "label", size: 1 }],
-    form_recipient_email: "example@example.com",
+    buttonLabel: "Sign up",
+    formRecipientEmail: "example@example.com",
   },
 };
