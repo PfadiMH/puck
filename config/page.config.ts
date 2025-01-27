46398e448e47e029bee6aa10264d3ae6f7ca11c3
Gabriel-A-NY <145563844+Gabriel-A-NY@users.noreply.github.com>
2025-01-27 08:32:03 +0100

    make flex component (#17)
    * make flex component
    
    * change namig
    
    * remove unused files and improve code consistency
    
    * make improvements regarding requested changes

--- config/page.config.ts
+++ config/page.config.ts
@@ -1,26 +1,28 @@
 import type { Config } from "@measured/puck";
-import { textConfig } from "@components/Text";
-import type { TextProps } from "@components/Text";
+import { textConfig, TextProps } from "@components/Text";
+import { formGroupConfig, FormGroupProps } from "@components/FormGroup";
 import { heroConfig, HeroProps } from "@components/Hero";
 import {
   verticalSpaceConfig,
   VerticalSpaceProps,
 } from "@components/VerticalSpace";
-import { formGroupConfig, FormGroupProps } from "@components/FormGroup";
+import { flexConfig, FlexProps } from "@components/Flex";
 
 type Props = {
   FormGroup: FormGroupProps;
   Hero: HeroProps;
   Text: TextProps;
   VerticalSpace: VerticalSpaceProps;
+  Flex: FlexProps;
 };
 
 export const config: Config<Props> = {
   components: {
     FormGroup: formGroupConfig,
-    Hero: heroConfig,
     Text: textConfig,
+    Hero: heroConfig,
     VerticalSpace: verticalSpaceConfig,
+    Flex: flexConfig,
   },
 };
 
