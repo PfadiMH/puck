0cbc34b3565774a1f65ccf72943107d0e49928a6
cediackermann <ackermanncedi@gmail.com>
2025-01-27 08:47:23 +0100

    merge with upstream/master

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,10 @@
-/** @type {import('tailwindcss').Config} */
+import type { Config } from "tailwindcss";
 
 const {
   default: flattenColorPalette,
 } = require("tailwindcss/lib/util/flattenColorPalette");
 
-module.exports = {
+const config: Config = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,6 +13,23 @@ module.exports = {
     extend: {
       boxShadow: {
         input: `0px 2px 3px -1px rgba(0,0,0,0.1), 0px 1px 0px 0px rgba(25,28,33,0.02), 0px 0px 0px 1px rgba(25,28,33,0.08)`,
+      },
+      fontFamily: {
+        rockingsoda: ["var(--rockingsoda)"],
+        poppins: ["var(--poppins)"],
+      },
+      colors: {
+        background: "var(--color-background)",
+        current: "var(--color-current)",
+        primary: "var(--color-primary)",
+        accent: "var(--color-accent)",
+        "brand-yellow": "#f4d51f",
+        "brand-red": "#be1622",
+      },
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
   },
@@ -29,3 +46,5 @@ function addVariablesForColors({ addBase, theme }: any) {
     ":root": newVars,
   });
 }
+
+export default config;
