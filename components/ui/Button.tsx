8056280197dfe3c8fc493af5075d88c9c704706c
Silvio Braendle <silvio@silviobraendle.ch>
2025-04-16 18:02:51 +0200

    Add size prop to Button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -2,15 +2,25 @@ import clsx from "clsx";
 import { ButtonHTMLAttributes, PropsWithChildren } from "react";
 import styles from "./Button.module.css";
 
+type ButtonProps = {
+  size?: "small" | "medium" | "large";
+};
+
 function Button({
   children,
+  size = "medium",
   ...props
-}: PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>>) {
+}: PropsWithChildren<ButtonHTMLAttributes<HTMLButtonElement>> & ButtonProps) {
   return (
     <button
       {...props}
       className={clsx(
-        "bg-primary text-background hover:bg-primary/90 active:bg-primary/80 px-5 py-2 font-semibold cursor-pointer",
+        "bg-primary text-background hover:bg-primary/90 active:bg-primary/80 font-semibold cursor-pointer",
+        {
+          "text-sm px-5 py-1 font-medium": size === "small",
+          "text-base px-5 py-2 font-semibold": size === "medium",
+          "text-lg px-7 py-3 font-bold": size === "large",
+        },
         styles.clip,
         props.className
       )}
