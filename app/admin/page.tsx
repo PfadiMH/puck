99e76b77de36ba04bfe01b357f8c68ec5ace9efd
Silvio Braendle <silvio@silviobraendle.ch>
2025-04-16 21:29:05 +0200

    Rework admin page

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,32 +1,27 @@
 "use client";
+import Button from "@components/ui/Button";
 import { deletePage, getAllPaths } from "@lib/db/database";
+import { queryClient } from "@lib/query-client";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
-import { useCallback, useEffect, useState } from "react";
+import { useState } from "react";
 
 export default function Page() {
-  const [pages, setPages] = useState<string[]>([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [newPagePath, setNewPageName] = useState("");
   const router = useRouter();
 
-  const loadPages = useCallback(async () => {
-    const fetchedPages = await getAllPaths();
-    setPages(fetchedPages);
-  }, []);
+  const { data: pages = [] } = useQuery({
+    queryKey: ["pages"],
+    queryFn: getAllPaths,
+  });
 
-  useEffect(() => {
-    loadPages();
-  }, [loadPages]);
-
-  const onDeleteClick = useCallback(
-    async (page: string) => {
-      if (confirm(`Are you sure you want to delete the page "${page}"?`)) {
-        await deletePage(page);
-        loadPages();
-      }
-    },
-    [loadPages]
-  );
+  const onDeleteClick = async (page: string) => {
+    if (confirm(`Are you sure you want to delete the page "${page}"?`)) {
+      await deletePage(page);
+      queryClient.invalidateQueries({ queryKey: ["pages"] });
+    }
+  };
 
   const handleAddPageClick = () => {
     setIsModalOpen(true);
@@ -48,33 +43,52 @@ export default function Page() {
 
   return (
     <div className="p-4">
-      <div className="flex justify-between items-center mb-4">
+      <div className="flex flex-wrap gap-2 justify-between mb-4">
         <h1>Leitereberiich</h1>
-        <div className="flex gap-4">
+        <div className="flex flex-wrap gap-4">
           <div className="grid grid-rows-2 gap-2">
-            <a href="/admin/navbar">
-              <button className="bg-yellow-500 hover:bg-yellow-700 text-white font-bold py-1 px-4 rounded w-full">
-                Navbar
-              </button>
-            </a>
-            <a href="/admin/footer">
-              <button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-1 px-4 rounded w-full">
-                Footer
-              </button>
-            </a>
+            <Button size="small" onClick={() => router.push("/admin/navbar")}>
+              Navbar
+            </Button>
+            <Button size="small" onClick={() => router.push("/admin/footer")}>
+              Footer
+            </Button>
           </div>
-          <button
-            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
-            onClick={handleAddPageClick}
-          >
-            Add Page
-          </button>
+          <Button onClick={handleAddPageClick}>Add Page</Button>
+        </div>
+      </div>
+
+      <div className="flex flex-col">
+        <div className="flex"></div>
+        <div className="flex-col flex gap-3">
+          {pages.map((page) => (
+            <div
+              key={page}
+              className="border-2 rounded-xl border-primary p-4 flex justify-between"
+            >
+              <div>{page}</div>
+              <div className="flex flex-wrap gap-3 justify-end">
+                <Button size="small" onClick={() => router.push(`${page}`)}>
+                  View
+                </Button>
+                <Button
+                  size="small"
+                  onClick={() => router.push(`/admin/editor${page}`)}
+                >
+                  Edit
+                </Button>
+                <Button size="small" onClick={() => onDeleteClick(page)}>
+                  Delete
+                </Button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
 
       {isModalOpen && (
-        <div className="fixed inset-0 bg-gray-500 bg-opacity-75 flex justify-center items-center">
-          <div className="bg-white p-6 rounded-lg">
+        <div className="fixed inset-0 flex justify-center items-center bg-black/50">
+          <div className="bg-background p-6 rounded-lg">
             <h2 className="text-lg font-bold mb-4">Add New Page</h2>
             <input
               type="text"
@@ -83,55 +97,17 @@ export default function Page() {
               value={newPagePath}
               onChange={(e) => setNewPageName(e.target.value)}
             />
-            <div className="flex justify-end gap-4">
-              <button
-                className="bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded"
-                onClick={handleCloseModal}
-              >
+            <div className="flex flex-wrap justify-end gap-4">
+              <Button size="small" onClick={handleCloseModal}>
                 Cancel
-              </button>
-              <button
-                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={handleCreatePage}
-              >
+              </Button>
+              <Button size="small" onClick={handleCreatePage}>
                 Create Page
-              </button>
+              </Button>
             </div>
           </div>
         </div>
       )}
-
-      <div className="flex flex-col">
-        <div className="flex flex-row"></div>
-        <div className="flex-col flex gap-3">
-          {pages.map((page) => (
-            <div
-              key={page}
-              className="border-2 rounded-xl border-[--primary-color] p-4 flex flex-row justify-between"
-            >
-              <div>{page}</div>
-              <div className="flex gap-3 flex-row">
-                <a href={`${page}`}>
-                  <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-                    View
-                  </button>
-                </a>
-                <a href={`/admin/editor${page}`}>
-                  <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                    Edit
-                  </button>
-                </a>
-                <button
-                  className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                  onClick={() => onDeleteClick(page)}
-                >
-                  Delete
-                </button>
-              </div>
-            </div>
-          ))}
-        </div>
-      </div>
     </div>
   );
 }
--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,8 +1,14 @@
 "use client";
 
+import { queryClient } from "@lib/query-client";
+import { QueryClientProvider } from "@tanstack/react-query";
 import { PropsWithChildren } from "react";
 import { ParallaxProvider } from "react-scroll-parallax";
 
 export function Providers({ children }: PropsWithChildren<unknown>) {
-  return <ParallaxProvider>{children}</ParallaxProvider>;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <ParallaxProvider>{children}</ParallaxProvider>
+    </QueryClientProvider>
+  );
 }
