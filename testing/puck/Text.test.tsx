d6d95784c6795f532d96e400534daf789592c5cf
cediackermann <ackermanncedi@gmail.com>
2025-12-08 10:14:53 +0100

    refactor: update test to use textConfig instead of headingConfig in Text.test.tsx

--- testing/puck/Text.test.tsx
+++ testing/puck/Text.test.tsx
@@ -1,16 +1,11 @@
-import { headingConfig } from "@components/puck/Heading";
+import { textConfig } from "@components/puck/Text";
 import { createPuckProps } from "@lib/testing/puckProps";
 import { testRender } from "@lib/testing/render";
 import { expect, test } from "vitest";
 
 test("renders", async () => {
   const screen = await testRender(
-    <headingConfig.render
-      {...createPuckProps()}
-      text="Hello World!"
-      textAlign="left"
-      level="h1"
-    />
+    <textConfig.render {...createPuckProps()} text="Hello World!" />
   );
 
   expect(screen.getByText("Hello World!")).toBeVisible();
