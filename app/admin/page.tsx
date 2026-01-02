cb5398b321b2b553beee2840bb12d673e53213ab
Battino <Battino46@gmail.com>
2026-01-02 23:48:57 +0100

    remove redundant session saves.

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -2,7 +2,7 @@ import AdminPage from "@components/page/admin/AdminPage";
 import { requirePageAuth } from "@lib/auth/auth-functions";
 
 export default async function Page() {
-  const session = await requirePageAuth(["admin-ui:read"]);
+  await requirePageAuth(["admin-ui:read"]);
 
   return <AdminPage />;
 }
