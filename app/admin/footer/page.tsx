541c506801dc4bb43b67e45cc931d17b8af0cf8a
Silvio Braendle <silvio@silviobraendle.ch>
2026-01-15 00:28:16 +0100

    Declare module for Puck css

--- app/admin/footer/page.tsx
+++ app/admin/footer/page.tsx
@@ -1,6 +1,5 @@
 import { FooterEditor } from "@components/footer/FooterEditor";
 import { getFooter } from "@lib/db/db-actions";
-import "@measured/puck/puck.css";
 import { Metadata } from "next";
 
 export async function generateMetadata(): Promise<Metadata> {
--- components/navbar/NavbarEditor.tsx
+++ components/navbar/NavbarEditor.tsx
@@ -8,6 +8,7 @@ import {
 } from "@lib/config/navbar.config";
 import { saveNavbar } from "@lib/db/db-actions";
 import { Puck } from "@measured/puck";
+import "@measured/puck/puck.css";
 
 export function NavbarEditor({ data }: { data: NavbarData }) {
   return (
