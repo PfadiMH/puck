49d8884729a1a3bc453d06081ef5f9bc515a14da
Abigail <abigail.llanque@edu.tbz.ch>
2026-01-30 10:49:26 +0100

    add Card component with vertical subslots->with 3 variants (elevated, outlined, filled),Add isNested flag to section theme context to prevent duplicate wrappers,23 browser tests and overflow-hidden to allow shadows to renderr

--- components/page/SectionThemedComponent.tsx
+++ components/page/SectionThemedComponent.tsx
@@ -1,4 +1,6 @@
+"use client";
 import { SectionThemeProvider } from "@components/contexts/SectionThemeProvider";
+import { useIsNestedTheme } from "@lib/contexts/section-theme-context";
 import { Theme } from "@lib/section-theming";
 import { PropsWithChildren } from "react";
 
@@ -6,9 +8,15 @@ export function SectionThemedComponent({
   children,
   theme,
 }: PropsWithChildren<{ theme: Theme }>) {
+  const isNested = useIsNestedTheme();
+
+  if (isNested) {
+    return <>{children}</>;
+  }
+
   return (
-    <SectionThemeProvider theme={theme}>
-      <div className={`${theme}-theme bg-ground content-main overflow-hidden`}>
+    <SectionThemeProvider theme={theme} isNested={true}>
+      <div className={`${theme}-theme bg-ground content-main`}>
         {children}
       </div>
     </SectionThemeProvider>
